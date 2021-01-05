32d19bc54fc07e5f02e67d46fc9bcce393f43b06
alicekykwan <62489895+alicekykwan@users.noreply.github.com>
2021-01-04 18:42:22 -0800

    mess around

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import { useState } from 'react';
+import AllBoards from './AllBoards';
 
 function App() {
-  let [ numBoards, setNumBoards ] = useState(5);
+  let [ gameSpeed, setGameSpeed ] = useState(3);
+  let [ coinsPerWin, setCoinsPerWin ] = useState(1);
+  let [ numBoards, setNumBoards ] = useState(3);
+  let [ coins, setCoins ] = useState(0);
 
-  let renderGame = (numBoards) => {
-    let res = []
-    for (let i =0; i< numBoards; i++) {
-      res.push('a game board')
-    }
-    return res
-  }
+  let addCoins = (numCoins) => { setCoins(coins + numCoins); }
 
   return (
     <div className="App">
-      <div className="game">
-       { renderGame(numBoards) }
-      </div>
+      <AllBoards
+        key="main"
+        gameSpeed={gameSpeed}
+        coinsPerWin={coinsPerWin}
+        numBoards={numBoards}
+        addCoins={addCoins} />
+      <br/>
+      <button onClick = {()=>setGameSpeed(gameSpeed+1)}> faster </button>
+      <button onClick = {()=>setGameSpeed(Math.max(gameSpeed-1, 1))}> slower </button>
+      <br/>
+      <button onClick = {()=>setCoinsPerWin(coinsPerWin+1)}> more coins per win </button>
+      <button onClick = {()=>setCoinsPerWin(coinsPerWin-1)}> fewer coins per win </button>
+      <br/>
       <button onClick = {()=>setNumBoards(numBoards+1)}> more boards </button>
-      <button onClick = {()=>setNumBoards(numBoards-1)}> fewer boards </button>
+      <button onClick = {()=>setNumBoards(Math.max(numBoards-1, 1))}> fewer boards </button>
+      <br/>
+      <div>
+        coins: {coins} <br />
+        game speed: {gameSpeed} <br />
+        coins per win: {coinsPerWin} <br />
+        num boards: {numBoards} <br />
+      </div>
     </div>
   );
 }
---
+++ src/GameState.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+function GameState() {
+  let gameSpeed = 10;
+  let coinsPerWin = 1;
+  let boards = [];
+  let coins = 0;
+  return this;
+}
+
+export default GameState;
